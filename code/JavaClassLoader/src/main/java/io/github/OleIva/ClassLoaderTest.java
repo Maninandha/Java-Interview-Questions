b7ba4031db0f1423a9003a58b4c0d85bde6af717
Oleh Ivashko <Oleg.Ivashko@yandex.ua>
2016-09-06 05:10:54 +0300

    add code of JavaClassLoader

---
+++ code/JavaClassLoader/src/main/java/io/github/OleIva/ClassLoaderTest.java
@@ -0,0 +1,11 @@
+package io.github.OleIva;
+
+public class ClassLoaderTest {
+
+    public static void main(String[] args) {
+        System.out.println("class loader for HashMap: " + java.util.HashMap.class.getClassLoader());
+        System.out.println("class loader for DNSNameService: " + sun.net.spi.nameservice.dns.DNSNameService.class.getClassLoader());
+        System.out.println("class loader for this class: " + ClassLoaderTest.class.getClassLoader());
+        System.out.println(com.mysql.jdbc.Blob.class.getClassLoader());
+    }
+}
